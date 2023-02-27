22f6c65b0cd249db9d05c8715177e441ebea2792
charlespaul1 <charlespaulwabomba@gmail.com>
2023-02-27 15:22:32 +0300

    aded form fields

--- dapwebsite/src/components/Form.js
+++ dapwebsite/src/components/Form.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Formik, Form, Field} from 'formik';
-import { FormControl, FormLabel, Input, Select, button } from '@chakra-ui/react'; 
-import {object, string, date} from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { FormControl, FormLabel, Input, Select, button } from '@chakra-ui/react';
+import { object, string, date } from 'yup';
 // setting my initial values as empty strings
 const initialValues = {
   Name: '',
-  PhoneNumber:'',
-  idOrPassport:'',
-  DateOfBirth:'',
-  DateOfRegistration:'',
-  Ward:'',
-  Constituency:'',
-  County:'',
+  PhoneNumber: '',
+  idOrPassport: '',
+  DateOfBirth: '',
+  DateOfRegistration: '',
+  Ward: '',
+  Constituency: '',
+  County: '',
 }
 // setting the form validation
 const validationSchema = object().shape({
@@ -27,7 +27,7 @@ const validationSchema = object().shape({
   County: string().required('County is required'),
 });
 //on submit function
-const onSubmit = (values, {setSubmitting}) =>{
+const onSubmit = (values, { setSubmitting }) => {
   console.log(values);
   setSubmitting(false);
 }
@@ -35,49 +35,105 @@ const onSubmit = (values, {setSubmitting}) =>{
 const regForm = () => {
   return (
     <Formik
-    initialValues={initialValues}
-    validationSchema={validationSchema}
-    onSubmit={onSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
     >
-      {({isSubmitting}) =>(
+      {({ isSubmitting }) => (
         <Form>
           <Field name='name'>
-            {({field, form}) => (
+            {({ field, form }) => (
               <FormControl
-              isInvalid={form.errors.name && form.touched.name}
-              mb="4"
+                isInvalid={form.errors.name && form.touched.name}
+                mb="4"
               >
                 <FormLabel htmlFor='name'>Name</FormLabel>
-                <Input {...field} id='name' placeholder='Name'/>
-                </FormControl>
+                <Input {...field} id='name' placeholder='Name' />
+              </FormControl>
             )}
           </Field>
           <Field name='phoneNumber'>
-            {({field, form}) => (
+            {({ field, form }) => (
               <FormControl
-              isInvalid={form.errors.phoneNumber && form.touched.phoneNumber}
-              mb='4'
+                isInvalid={form.errors.phoneNumber && form.touched.phoneNumber}
+                mb='4'
               >
                 <FormLabel htmlFor='phoneNumber'>Phone Number</FormLabel>
-                <Input {...field} id='phoneNumber' placeholder='Phone Number'/>
+                <Input {...field} id='phoneNumber' placeholder='Phone Number' />
               </FormControl>
             )}
           </Field>
           <Field name='idOrPassport'>
-            {({field, form}) => (
+            {({ field, form }) => (
               <FormControl
-              isInvalid={form.errors.idOrPassport && form.touched.idOrPassport}
-              mb='4'
+                isInvalid={form.errors.idOrPassport && form.touched.idOrPassport}
+                mb='4'
               >
-              <FormLabel htmlFor='ID/passport'>ID/Passport</FormLabel>
-              <Input {...field} id='idOrPassport' placeholder='Id or Passport'/>
+                <FormLabel htmlFor='ID/passport'>ID/Passport</FormLabel>
+                <Input {...field} id='idOrPassport' placeholder='Id or Passport' />
               </FormControl>
             )}
-
           </Field>
+          <Field name='dateOfBirth'>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.DateOfBirth && form.touched.DateOfBirth}
+                mb='4'
+              >
+                <FormLabel htmlFor='dateOfBirth'>Date Of Birth</FormLabel>
+                <Input {...field} id='dateOfBirth' placeholder='Date of Birth' />
+              </FormControl>
+            )}
+          </Field>
+          <Field name='dateOfRegistration'>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.DateOfRegistration && form.touched.DateOfRegistration}
+                mb='4'
+              >
+                <FormLabel htmlFor='dateOfRegistration'>Date Of Registration</FormLabel>
+                <Input {...field} id='dateOfRegistration' placeholder='Date of Registration' />
+              </FormControl>
+            )}
+          </Field>
+          <Field name='wardd'>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.Ward && form.touched.Ward}
+                mb='4'
+              >
+                <FormLabel htmlFor='ward'>Ward</FormLabel>
+                <Input {...field} id='ward' placeholder='ward' />
+              </FormControl>
+            )}
+          </Field>
+          <Field name='constituency'>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.Constituency && form.touched.Constituency}
+                mb='4'
+              >
+                <FormLabel htmlFor='constituency'>Constituency</FormLabel>
+                <Input {...field} id='constituency' placeholder='constituency' />
+              </FormControl>
+            )}
+          </Field>
+          <Field name='county'>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.county && form.touched.county}
+                mb='4'
+              >
+                <FormLabel htmlFor='county'>County</FormLabel>
+                <Input {...field} id='county' placeholder='county' />
+              </FormControl>
+            )}
+          </Field>
+         
+        
         </Form>
       )}
-      
+
     </Formik>
   )
 }
