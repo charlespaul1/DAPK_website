4b79f71ab614de1d819451cad5d122f9bababebe
charlespaul1 <charlespaulwabomba@gmail.com>
2023-06-22 22:09:02 +0300

    added a responsive home page

--- dac_website/src/pages/home.jsx
+++ dac_website/src/pages/home.jsx
@@ -1,9 +1,29 @@
-
+import Logo from '../assets/logo.jpg'
 import Header from '../components/header'
+import './home.css'
 const HomePage = () => {
   return (
     <>
     <Header/>
+    <div className="home">
+      <div className='top-home'>
+      <img src={Logo} alt='logo' className='logo' 
+      // style={{width: '300px', height: '300px', borderRadius: '50%'}}
+      />
+      <h1>DEMOCRATIC ACTION COALITION PARTY</h1>
+      </div>
+      <div className='bottom-home'>
+      <h3>The New Dawn has come!!!!</h3>
+      <section>
+        <p>Join us in making a difference.</p>
+        <a href='#register'>
+          <button>Register</button>
+        </a>
+      </section>
+
+      </div>
+     
+    </div>
     
     </>
   );
